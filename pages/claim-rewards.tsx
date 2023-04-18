5829ea964d7e12bbb863574821a3937960aaefa0
Mauricio Henao <atomauro@cosmos-2.local>
2023-04-17 22:03:46 -0500

    .

--- pages/claim-rewards.tsx
+++ pages/claim-rewards.tsx
@@ -7,12 +7,15 @@ import { useState } from 'react'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import { HiCheckCircle } from 'react-icons/hi'
 
+import { JigsawPuzzle } from 'react-jigsaw-puzzle/lib'
+import 'react-jigsaw-puzzle/lib/jigsaw-puzzle.css'
+
 const DEFAULT_LAYOUT = 'RewardLayout'
 
 const questions = [
   {
     id: 1,
-    question: 'What is an NFT?',
+    question: 'Please organize the NABShow logo',
     answers: [
       { id: 'A', text: 'A type of cryptocurrency' },
       {
@@ -23,16 +26,6 @@ const questions = [
     ],
     correctAnswer: 'B',
   },
-  {
-    id: 2,
-    question: 'What blockchain are most NFTs built on?',
-    answers: [
-      { id: 'A', text: 'Ethereum' },
-      { id: 'B', text: 'Bitcoin' },
-      { id: 'C', text: 'Cardano' },
-    ],
-    correctAnswer: 'A',
-  },
 ]
 
 export const getStaticProps = async () => {
@@ -42,15 +35,11 @@ export const getStaticProps = async () => {
 
 export default function About({ author }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [answers, setAnswers] = useState({})
+  const [isCorrectAnswer, setisCorrectAnswer] = useState(false)
 
   const handleAnswerChange = (questionId: number, answerId: string) => {
     setAnswers((prevAnswers) => ({ ...prevAnswers, [questionId]: answerId }))
     console.log('RESPUESTAS', answers)
-  }
-
-  const handleClaim = () => {
-    // Check answers and give rewards
-    console.log(answers)
   }
 
   return (
@@ -67,85 +56,71 @@ export default function About({ author }: InferGetStaticPropsType<typeof getStat
         <div className="prose max-w-none pb-8 text-justify text-sm dark:prose-dark md:text-lg xl:col-span-2">
           Take a journey on these few questions then connect or create a wallet not worry if you
           dont have one we are going to guide you click on add network and add OHMIO Token then
-          click on Claim to finally have your tokens
+          click on Claim to finally have your tokens.
         </div>
       </div>
 
-      {questions.map(({ id, question, answers, correctAnswer }) => {
-        const selectedAnswer = answers[id]
-        const isCorrectAnswer = selectedAnswer && selectedAnswer.id == correctAnswer
+      <dl
+        style={{
+          position: 'fixed',
+          bottom: '9rem',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <dd>
+          <div className="flex items-center justify-center">
+            <span style={{ textDecoration: 'underline' }}>Your answer is</span>
+          </div>
+          {isCorrectAnswer ? (
+            <span
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#2F9F21',
+                borderRadius: 50,
+                padding: '1rem',
+              }}
+            >
+              <HiCheckCircle color="#FFFFFF" style={{ marginRight: '0.4rem' }} /> Correct, claim
+              rewards
+            </span>
+          ) : (
+            <span
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#9B9C9C',
+                borderRadius: 50,
+                padding: '1rem',
+                width: '15rem',
+              }}
+            >
+              <AiFillCloseCircle color="#FFFFFF" style={{ marginRight: '0.4rem' }} /> Not finished
+            </span>
+          )}
+        </dd>
+      </dl>
+
+      {questions.map(({ id }) => {
         return (
           <header
             className="my-10 space-y-1 rounded-lg bg-primary-500 py-4 px-10 text-center sm:py-6 md:py-10"
             key={id}
           >
-            <PageTitle>Question #{id}</PageTitle>
-            <div>
-              <h1 className="text-2xl font-bold leading-9 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-                {question}
-              </h1>
-            </div>
-            {answers.map(({ id: answerId, text }) => (
-              <div key={text}>
-                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                  <h1 className="text-2xl font-bold leading-9 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-                    {answerId}
-                    {') '}
-                    <span style={{ fontWeight: 'normal' }}>{text}</span>
-                  </h1>
-                  <input
-                    type="checkbox"
-                    style={{
-                      width: '2rem',
-                      height: '2rem',
-                      borderRadius: '30px',
-                      color: '#eb008b',
-                    }}
-                    checked={answers[id].id == answerId}
-                    onChange={() => handleAnswerChange(id, answerId)}
-                  />
-                </div>
-              </div>
-            ))}
-            <dl>
-              <dd className="flex flex-col justify-center text-2xl text-base font-medium leading-6 text-white sm:flex-row sm:space-x-1">
-                <div className="flex items-center justify-center space-x-1">
-                  <span style={{ textDecoration: 'underline' }}>Your answer is</span>
-                </div>
-                {isCorrectAnswer ? (
-                  <span
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      backgroundColor: '#2F9F21',
-                      borderRadius: 50,
-                      padding: '1rem',
-                      marginLeft: '2rem',
-                    }}
-                  >
-                    <HiCheckCircle color="#FFFFFF" style={{ marginRight: '0.4rem' }} /> Correct
-                  </span>
-                ) : (
-                  <span
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      backgroundColor: '#9B9C9C',
-                      borderRadius: 50,
-                      padding: '1rem',
-                      marginLeft: '2rem',
-                    }}
-                  >
-                    <AiFillCloseCircle color="#FFFFFF" style={{ marginRight: '0.4rem' }} />{' '}
-                    Incorrect
-                  </span>
-                )}
-              </dd>
-            </dl>
+            <PageTitle>Organize the NAB Show logo</PageTitle>
+
+            <JigsawPuzzle
+              imageSrc={'/static/images/nabshow.png'}
+              rows={1}
+              columns={2}
+              onSolved={() => setisCorrectAnswer(true)}
+            />
           </header>
         )
       })}
